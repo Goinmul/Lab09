dc78a5fe1af340680ace4b0c11fa97279617f696
Goinmul <21400045@handong.edu>
2018-04-12 21:49:20 +0900

    Hw9. Having some problems.

--- src/main/java/edu/handong/csee/java/lab09/prob1/SalesReport.java
+++ src/main/java/edu/handong/csee/java/lab09/prob1/SalesReport.java
@@ -5,14 +5,13 @@ import java.util.Arrays; // I didn't use the array packages that other programme
  * This class is about making sales report.
  * I used constructor on it.
  * @author HW
- *
  */
 public class SalesReport {
-	
-	private int highestSales = 0;
+
+	private double highestSales = 0;
 	private int numOfSalesman = 0;
 	private SalesReport[] team;
-	
+	private double averageSales=0;
 	private String name;
 	private double salesNum = 0.0;
 
@@ -22,48 +21,86 @@ public class SalesReport {
 	}
 
 	public void getData() {
-		
-		System.out.println("Enter number of sales associate:"); // getting associate number
+
+		System.out.print("Enter number of sales associate:"); // getting associate number
 		Scanner myScanner = new Scanner(System.in);
 		numOfSalesman = myScanner.nextInt();
 
-		
+		double temp1 = 0.0;
+		//SalesReport mySales = new SalesReport();
+
 		for (int i = 0; i < numOfSalesman; i++) {
-			
-			System.out.println("Enter data for associate number "+(i+1));
-			
+
+			System.out.print("Enter data for associate number "+(i+1));
+
 			System.out.print("Enter name of sales associate: ");
 			String name1 = myScanner.nextLine();
-			team[i].name = name1;
-			
+			team[i].setName(name1);
+
 			System.out.print("Enter associate's sales : $");
 			double sales1 = myScanner.nextDouble();
-			team[i].salesNum = sales1;
-			
+			team[i].setSalesNum(sales1);
+
+			if(sales1 > temp1) {
+
+				setHighestSales(sales1);
+				temp1 = sales1;
+
+			}
+
 		}
 	}
-	
-	public void CalculateAverage(SalesReport[] team) {
+
+	public void CalculateAverage() {
 
 		double calSum = 0.0;
 		numOfSalesman = getNumOfSalesman();
-		
+
 		for (int i=0; i<numOfSalesman;i++) {
 			calSum += team[i].salesNum;
 		}
-		
+
 		double average1 = calSum/numOfSalesman;
-		System.out.print("Average sales is : "+calSum);
+		System.out.print("Average sales is : "+ calSum);
+
+		setAverageSales(average1);
+		//return average1;
 	}
 
-	
-	public static void main (String[] args) {
-		SalesReport rpt = new SalesReport();
-		
-		rpt.getData();
-		rpt.CalculateAverage();
-		
-	
+	public void HighestSales() {
+		System.out.print("Average sales per associate is $" + averageSales); // I want to get access to the method above's variable average1,...
+		System.out.print("The highest sales figure is $"+ highestSales +".");
+
+		System.out.print("The following had the highest sales");
+
+		for (int i = 0; i<numOfSalesman;i++) {
+			System.out.print("Name : "+ team[i].getName());
+			System.out.print("Sales : $" + team[i].getSalesNum());
+			if (team[i].getSalesNum()<getHighestSales())
+				System.out.print("$"+(getAverageSales()-team[i].getSalesNum())+" below the average.");
+
+			else {
+				System.out.print("$"+(team[i].getSalesNum()-getAverageSales())+" above the avergae.");
+			}
+		}
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalesNum() {
+		return salesNum;
+	}
+
+	public void setSalesNum(double salesNum) {
+		this.salesNum = salesNum;
 	}
 
 	public int getNumOfSalesman() {
@@ -74,9 +111,30 @@ public class SalesReport {
 		this.numOfSalesman = numOfSalesman;
 	}
 
+	public double getAverageSales() {
+		return averageSales;
+	}
 
-	
-	
-	
-	
+	public void setAverageSales(double averageSales) {
+		this.averageSales = averageSales;
+	}
+
+	public double getHighestSales() {
+		return highestSales;
+	}
+
+	public void setHighestSales(double highestSales) {
+		this.highestSales = highestSales;
+	}
+
+
+
+	public static void main (String[] args) {
+		SalesReport rpt = new SalesReport();
+
+		rpt.getData();
+		rpt.CalculateAverage();
+		rpt.HighestSales();
+
+	}
 }
